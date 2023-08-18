757744e483cdcd424e4a9139847bf5a78b131295
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2023-08-18 07:17:51 +0200

    base logic to solve sudoku

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,26 @@
 class SudokuSolver {
+  validate(puzzleString) {}
 
-  validate(puzzleString) {
-  }
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkColPlacement(puzzleString, row, column, value) {
-
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
   solve(puzzleString) {
-    
+    // create and two-dimentional array (9x9 size)
+    // check if puzzle is solved (are there any dots in an array?)
+    // if puzzle is solved:
+    // return true
+    // if puzzle is not solved:
+    // find the first dot in the array
+    // create a loop from 1 to 9 to replace the dot
+    // if row, col, square check is true:
+    // recurse the function
+    // if row, col, square check is false:
+    // try next num in the loop
+    // if all nums don't pass the check backtrack to previous recursion
   }
 }
 
 module.exports = SudokuSolver;
-
