3ace660dc7739274e3d28f7464e8d57d0f401e88
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2023-08-20 08:44:49 +0200

    index.js, api.js, package-lock.json

--- public/index.js
+++ public/index.js
@@ -4,8 +4,8 @@ const valInput = document.getElementById("val");
 const errorMsg = document.getElementById("error");
 
 document.addEventListener("DOMContentLoaded", () => {
-  textArea.value =
-    "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+  textArea.value = "..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..";
+  // textArea.value = "..9..5.1.85.411112432111111111169183191111161621711119111111194511114137141311611";
   fillpuzzle(textArea.value);
 });
 
@@ -16,8 +16,8 @@ textArea.addEventListener("input", () => {
 function fillpuzzle(data) {
   let len = data.length < 81 ? data.length : 81;
   for (let i = 0; i < len; i++) {
-    let rowLetter = String.fromCharCode('A'.charCodeAt(0) + Math.floor(i / 9));
-    let col = (i % 9) + 1; 
+    let rowLetter = String.fromCharCode("A".charCodeAt(0) + Math.floor(i / 9));
+    let col = (i % 9) + 1;
     if (!data[i] || data[i] === ".") {
       document.getElementsByClassName(rowLetter + col)[0].innerText = " ";
       continue;
@@ -28,37 +28,36 @@ function fillpuzzle(data) {
 }
 
 async function getSolved() {
-  const stuff = {"puzzle": textArea.value}
+  const stuff = { puzzle: textArea.value };
   const data = await fetch("/api/solve", {
     method: "POST",
     headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
+      Accept: "application/json",
+      "Content-type": "application/json",
     },
-    body: JSON.stringify(stuff)
-  })
+    body: JSON.stringify(stuff),
+  });
   const parsed = await data.json();
   if (parsed.error) {
     errorMsg.innerHTML = `<code>${JSON.stringify(parsed, null, 2)}</code>`;
-    return
+    return;
   }
-  fillpuzzle(parsed.solution)
+  fillpuzzle(parsed.solution);
 }
 
 async function getChecked() {
-  const stuff = {"puzzle": textArea.value, "coordinate": coordInput.value, "value": valInput.value}
-    const data = await fetch("/api/check", {
+  const stuff = { puzzle: textArea.value, coordinate: coordInput.value, value: valInput.value };
+  const data = await fetch("/api/check", {
     method: "POST",
     headers: {
-      "Accept": "application/json",
-      "Content-type": "application/json"
+      Accept: "application/json",
+      "Content-type": "application/json",
     },
-    body: JSON.stringify(stuff)
-  })
+    body: JSON.stringify(stuff),
+  });
   const parsed = await data.json();
   errorMsg.innerHTML = `<code>${JSON.stringify(parsed, null, 2)}</code>`;
 }
 
-
-document.getElementById("solve-button").addEventListener("click", getSolved)
-document.getElementById("check-button").addEventListener("click", getChecked)
+document.getElementById("solve-button").addEventListener("click", getSolved);
+document.getElementById("check-button").addEventListener("click", getChecked);
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,14 @@
-'use strict';
-
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+"use strict";
+const SudokuSolver = require("../controllers/sudoku-solver.js");
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route("/api/solve").post((req, res) => {
+    let puzzle2D = solver.createArrayFromString(req.body.puzzle.split("").map((x) => (x === "." ? "." : parseInt(x))));
+    let result = solver.solve(puzzle2D);
+    console.log("result ======>", result);
+  });
 };
