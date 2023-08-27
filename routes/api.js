7bef205148938fdc7cd026f18c7525efa36402a9
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2023-08-27 07:06:51 +0200

    minor changes

--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,16 @@ module.exports = function (app) {
   let solver = new SudokuSolver();
 
   app.route("/api/check").post((req, res) => {
-    let validated = solver.validate(req.body.puzzle);
+    let { puzzle, coordinate, value } = req.body;
+    if (!puzzle || !coordinate || !value) return res.json({ error: "Required field(s) missing" });
+    if (coordinate.length !== 2 || !/[A-I]/i.test(coordinate[0]) || !/[1-9]/.test(coordinate[1])) return res.json({ error: "Invalid coordinate" });
+    if (!/^[1-9]$/.test(value)) return res.json({ error: "Invalid value" });
+    let validated = solver.validate(puzzle);
     if (!Array.isArray(validated)) return res.json({ error: validated });
-    let [row, col, val] = [["A", "B", "C", "D", "E", "F", "H", "G", "I"].indexOf(req.body.coordinate[0]), req.body.coordinate[1] - 1, parseInt(req.body.value)];
+    let [row, col, val] = [["A", "B", "C", "D", "E", "F", "G", "H", "I"].indexOf(coordinate[0]), coordinate[1] - 1, parseInt(value)];
     let checkCoordinate = solver.checkPlacement(validated, row, col, val);
-    console.log("====>", checkCoordinate);
     return Array.isArray(checkCoordinate) ? res.json({ valid: false, conflict: checkCoordinate }) : res.json({ valid: true });
   });
-
-  //   You can POST to /api/check an object containing puzzle, coordinate, and value where the coordinate is the letter A-I indicating the row, followed by a number 1-9 indicating the column, and value is a number from 1-9.
-  // Failed:The return value from the POST to /api/check will be an object containing a valid property, which is true if the number may be placed at the provided coordinate and false if the number may not. If false, the returned object will also contain a conflict property which is an array containing the strings "row", "column", and/or "region" depending on which makes the placement invalid.
-  // Failed:If value submitted to /api/check is already placed in puzzle on that coordinate, the returned value will be an object containing a valid property with true if value is not conflicting.
-  // Failed:If the puzzle submitted to /api/check contains values which are not numbers or periods, the returned value will be { error: 'Invalid characters in puzzle' }
-  // Failed:If the puzzle submitted to /api/check is greater or less than 81 characters, the returned value will be { error: 'Expected puzzle to be 81 characters long' }
-  // Failed:If the object submitted to /api/check is missing puzzle, coordinate or value, the returned value will be { error: 'Required field(s) missing' }
-  // Failed:If the coordinate submitted to api/check does not point to an existing grid cell, the returned value will be { error: 'Invalid coordinate'}
-  // Failed:If the value submitted to /api/check is not a number between 1 and 9, the returned value will be { error: 'Invalid value' }
 
   app.route("/api/solve").post((req, res) => {
     let validated = solver.validate(req.body.puzzle);
