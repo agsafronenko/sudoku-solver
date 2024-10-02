785baf9c9bf385d4ba31a30488a6a5d10fccad3b
agsafronenko <109076625+agsafronenko@users.noreply.github.com>
2024-10-02 17:22:39 +0200

    func test example

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,17 +6,16 @@ const server = require("../server");
 chai.use(chaiHttp);
 
 suite("Functional Tests", () => {
-  // let validPuzzle = "759236.1.816495..2432178...174569...395842.6.62......9......1.45....4..79473..6..";
-  //   test("validPuzzle is a string", function (done) {
-  //     chai
-  //     .request(server)
-  //     .post("/api/solve")
-  //     .send({puzzle: validPuzzle})
-  //     .end(function(err, res) {
-  //       assert.typeOf(res.body.solution.length, "number")
-  //     })
-  //   })
+  let validPuzzle = "759236.1.816495..2432178...174569...395842.6.62......9......1.45....4..79473..6..";
+    test("validPuzzle is a string", function (done) {
+      chai
+      .request(server)
+      .post("/api/solve")
+      .send({puzzle: validPuzzle})
+      .end(function(err, res) {
+        assert.typeOf(res.body.solution.length, "number")
+      })
+    })
  
-  // });
+  });
 
-});
